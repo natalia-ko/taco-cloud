168f3f70463499dab856d1b82bd64fed092b8b03
NataKo <natalia.kozhukhova@ya.ru>
2021-05-06 23:43:13 +0300

    Initial commit

---
+++ src/main/java/com/springinaction/tacocloud/Taco.java
@@ -0,0 +1,14 @@
+package com.springinaction.tacocloud;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+    private List<String> ingredients;
+
+}
+
