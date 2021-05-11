06fb5e0ce62bc7866f21dbce3656bdc7572561bc
NataKo <natalia.kozhukhova@ya.ru>
2021-05-11 21:44:24 +0300

    Chapter2_2: updated OrderController and class Order, added orderForm template

--- src/main/java/com/springinaction/tacocloud/controller/DesignTacoController.java
+++ src/main/java/com/springinaction/tacocloud/controller/DesignTacoController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -42,11 +43,20 @@ public class DesignTacoController {
 
         return "design";
     }
-//added to resolve filterByType() method. Not from a book
+
+    //added to resolve filterByType() method. Not from a book
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients.stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processDesign(Taco design) {
+        //save
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
     }
 
 
